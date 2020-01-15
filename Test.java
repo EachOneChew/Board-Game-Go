1b1f7f2bbd5b73672cbb30ba93f4c53a2deb274e
Yichuan Qiu <54992471+EachOneChew@users.noreply.github.com>
2020-01-15 16:08:48 -0500

    Added tests

--- Test.java
+++ Test.java
@@ -12,5 +12,38 @@ class Main
     int size2 = testBoard2.getSize();
     int size3 = testBoard3.getSize();
     
+    // place pieces in a large square around edges of board to test edge case
     for (int i = 1; i < size1 - 1; i++)
-    {    
+    {
+      for (int j = 1; j < size1 - 1; j++)
+      {
+        assert testBoard1.placePiece(i, j, 1) : " cannot place";
+      }
+    }
+    
+    for (int i = 0; i < size1; i++)
+    {
+      assert testBoard1.placePiece(0, i, 2) : " cannot place";
+    }
+    
+    for (int i = 0; i < size1; i++)
+    {
+      assert testBoard1.placePiece(size1 - 1, i, 2) : " cannot place";
+    }
+    
+    for (int i = 1; i < size1 - 1; i++)
+    {
+      assert testBoard1.placePiece(i, 0, 2) : " cannot place";
+      assert testBoard1.placePiece(i, size1 - 1, 2) : " cannot place";
+    }
+    
+    // check that all pieces captured
+    for (int i = 1; i < size1 - 1; i++)
+    {
+      for (int j = 1; j < size1 - 1; j++)
+      {
+        assert testBoard1.getBoard()[i][j] == 0 : " uncaptured piece";
+      }
+    }
+  }
+}
